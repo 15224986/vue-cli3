34fcff53a11e85bc69ad6d82d9276f42fa9e5dde
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-04-29 16:41:05 +0800

    Update deepCopy.js

--- src/mixins/deepCopy.js
+++ src/mixins/deepCopy.js
@@ -3,17 +3,25 @@ export default {
 		/**
 		 * obj对象/、arr数组的深层复制
 		 */
-		setDeepCopy(obj){
-			var newObj = obj.constructor === Array ? []:{};
+		setDeepCopy(obj) {
+			if (!obj) {
+				return false;
+			}
+			var newObj = obj.constructor === Array ? [] : {};
 			// 实现深层复制
-			if( obj.constructor === Array ){
-				newObj = newObj.concat( obj );
-			}else if( obj.constructor === Object ){
-				for(var key in obj){
-					// 判断如果是对象，且不是null
-					if( typeof obj[key] === 'object' ){
-						// 因为null没有constructor属性，所以必须将null放在前面。
-						if( obj[key] === null || obj[key].constructor === RegExp || obj[key].constructor === Date ){
+			if (obj.constructor === Array) {
+				for(var i=0;i<obj.length;i++){
+					let item = obj[i];
+					if( item.constructor === Array || item.constructor === Object ){
+						item = this.setDeepCopy(item);
+					}
+					newObj.push(item);
+				}
+			} else if (obj.constructor === Object) {
+				for (var key in obj) {
+					// 判断如果是对象
+					if (typeof obj[key] === 'object') {
+						if (obj[key] === null || obj[key].constructor === RegExp || obj[key].constructor === Date) {
 							newObj[key] = obj[key];
 						} else {
 							// 递归
@@ -23,7 +31,7 @@ export default {
 						newObj[key] = obj[key];
 					}
 				}
-			}else{
+			} else {
 				newObj = obj;
 			}
 			return newObj;
