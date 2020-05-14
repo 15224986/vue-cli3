fbdf14217f34c5b214835b554ec74745e768c63c
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-05-14 09:51:58 +0800

    Update mockjsData.vue

--- src/views/index/mockjsData.vue
+++ src/views/index/mockjsData.vue
@@ -10,36 +10,50 @@
                 <h3 class="section-title">mock.js 测试</h3>
                 <ul class="section-subhead">
                     <li>1.制作mock.js案例，和使用方式方法。</li>
+                    <div class="section-subhead-tools">
+                        <el-button @click="exportExcel( $refs.exportExcel.$el, '资产报表.xlsx')" type="primary" plain><i class="el-icon-download"></i>导出 Excel</el-button>
+                    </div>
                 </ul>
                 <div class="table-box">
                     <el-table
                         :data="table"
                         border
-                        stripe 
+                        stripe
                         size="mini"
                         class="text-center"
                         header-row-class-name="el-table-th"
+                        ref="exportExcel"
                     >
-                        <el-table-column label="ID" property="id" width="80"></el-table-column>
+                        <el-table-column label="排序" type="index" width="50" fixed="left"></el-table-column>
                         <el-table-column label="名字" property="cname" width="100"></el-table-column>
-                        <el-table-column label="账号" property="username" width="186"></el-table-column>
-                        <el-table-column label="户口所在地" property="county" min-width="186"></el-table-column>
+                        <el-table-column label="账号" property="username" width="186" show-overflow-tooltip></el-table-column>
+                        <el-table-column label="户口所在地" property="county" min-width="186" show-overflow-tooltip></el-table-column>
                         <el-table-column label="常用软件" property="maliceIdType" width="126"></el-table-column>
                         <el-table-column label="星座" property="constellation" width="88"></el-table-column>
                         <el-table-column label="出生日期" property="recognitionTime" width="126"></el-table-column>
-                        <el-table-column label="个人主页" property="url" min-width="186"></el-table-column>
+                        <el-table-column label="个人主页" property="url" min-width="186" show-overflow-tooltip></el-table-column>
                         <el-table-column label="说说" property="csentence" min-width="186" show-overflow-tooltip></el-table-column>
-                        <el-table-column label="日记" property="csentence" min-width="186"></el-table-column>
+                        <el-table-column label="日记" property="csentence" min-width="186" show-overflow-tooltip></el-table-column>
                     </el-table>
                 </div>
-            </section>             
+            </section>
         </div>
 
     </article>
 </template>
 <script>
+    /**
+     * 混入
+     * calcIndex    : 序号排序的过滤方法
+     * exportExcel  : 导出Excel的方法
+     */
+    import calcIndex from "@/mixins/calcIndex.js";
+    import exportExcel from "@/mixins/exportExcel.js";
+
+
     export default {
         name: "mockjsData",
+        mixins: [calcIndex, exportExcel],
         data() {
             return {
                 loading: false,
@@ -50,7 +64,7 @@
             };
         },
         created() {
-            
+
             this.initData()
         },
         methods: {
@@ -92,7 +106,13 @@
         padding: 0 0 10px;
     }
     .section-subhead{
-        padding-bottom: 16px;
+        padding-bottom: 36px;
+        position: relative;
+    }
+    .section-subhead-tools{
+        position: absolute;
+        right: 0;
+        top: 0;
     }
     .section-title + .section-subhead{
         margin-top: -6px;
