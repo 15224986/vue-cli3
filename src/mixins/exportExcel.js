4a201ea7bdd7b65fc3a471f437ae950d0bebdde6
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-05-14 09:49:27 +0800

    Add files via upload

---
+++ src/mixins/exportExcel.js
@@ -0,0 +1,29 @@
+export default {
+	methods:{
+		exportExcel(tableDom, xlsxName){
+		    // generate workbook object from table
+		    // 从表生成工作簿对象
+		    let tableData = this.$XLSX.utils.table_to_book( tableDom );
+		    // get binary string as output
+		    // 获取二进制字符串作为输出
+		    let wbout = this.$XLSX.write( tableData, {type:'array'});
+		    try {
+		        this.$FileSaver.saveAs(new Blob([wbout]), xlsxName);
+		    }catch(err){
+		        console.log(err)
+		    }
+		
+		    // /* generate workbook object from table */
+		    // let wb =this.$XLSX.utils.table_to_book(document.querySelector('#downqueryshareaccessrpttable'));
+		    // /* get binary string as output */
+		    // let wbout = this.$XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'array' });
+		    // try {
+		    //     this.$FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }), '资产报表.xlsx');
+		    // } catch (e){
+		    //     if (typeof console !== 'undefined')
+		    //     console.log(e, wbout)
+		    // }
+		    // return wbout
+		},
+	}
+}
