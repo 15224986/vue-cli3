2b895f260046db13665767e77ffa0a71597868fb
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-05-15 13:48:19 +0800

    Update jQuery.vue

--- src/views/index/jQuery.vue
+++ src/views/index/jQuery.vue
@@ -8,14 +8,14 @@
 
             <section class="section" v-mocTitle="$route.meta.title">
                 <h2 id="jq">测试是否引入了jq</h2>
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <h2>
                     <router-link :to="{path: '/home/requiredType', query:{ title:'我是通过 touter-link 到来的'}}">
                         <span>切换必填状态 测试 tagsView 显示的文字，通过参数获取</span>
                     </router-link>
                 </h2>
-                <br/>
+                <br />
             </section>
 
             <section class="section" v-mocTitle="'数组de方法'">
@@ -46,6 +46,190 @@
                 <h4>截取字符串： <span class="bgc">{{string5}}</span></h4>
             </section>
 
+            <section class="section" v-mocTitle="'数组de方法'">
+                <h2 id="array">Lodash</h2>
+                <ul class="section-subhead">
+                    <li>1.Lodash 通过降低 array、number、objects、string 等等的使用难度从而让 JavaScript 变得更简单。</li>
+                    <li>2.参考网址： <el-link href="https://www.lodashjs.com" type="primary" target="_blank">https://www.lodashjs.com</el-link></li>
+                    <li>3.安装相关依赖： npm i --save lodash</li>
+                </ul>
+
+                <el-row :gutter="15" class="lodash-container">
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header">
+                                <span>去除数组array中的最后一个元素</span>
+                            </div>
+                            <div class="lodash-content">
+                                this.$lodash.initial([1, 2, 3])
+                                <br />
+                                // => [1, 2]
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header">
+                                <span>返回数组 array的第一个元素</span>
+                            </div>
+                            <div class="lodash-content">
+                                this.$lodash.head([1, 2, 3])
+                                <br />
+                                // => 1
+                            </div>
+                        </el-card>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>返回数组 array的最后一个元素</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.last(['a', 'b', 'c']);
+                                <br />
+                                // => 'c'
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>合并数组</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.concat([1],[2])
+                                <br />
+                                // => [1,2]
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>左切片</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.drop([1, 2, 3],2切除的数量)
+                                <br />
+                                // => [3]
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>右切片</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.dropRight([1, 2, 3],2切除的数量)
+                                <br />
+                                // => [1]
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>修改拼接</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.join(['a', 'b', 'c'], '~');
+                                <br />
+                                // => 'a~b~c'
+                            </div>
+                        </el-card>
+                    </el-col>
+
+
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>数组去重</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.uniq(['a', 'b', 'a']);
+                                <br />
+                                // => ['a','b']
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>获取数组的最大值</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.max([4, 2, 8, 6])
+                                <br />
+                                // => 8
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>获取数组的最小值</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.min([4, 2, 8, 6])
+                                <br />
+                                // => 2
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header">
+                                <span>四舍五入(保留任意位小数)</span>
+                            </div>
+                            <div class="lodash-content">
+                                this.$lodash.round(4.006,2保持几位小数)
+                                <br />
+                                // => 4.01
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>数组内数据相加</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.sum([4, 2, 8, 6])
+                                <br />
+                                // => 20
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>返回随机数</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.random(0, 5)
+                                <br />
+                                // => 0到5任意数
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>返回数组内的随机数</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.sample([1, 2, 3, 4])
+                                <br />
+                                // => 数组1到4任意数
+                            </div>
+                        </el-card>
+                    </el-col>
+
+                    <el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="6">
+                        <el-card shadow="hover">
+                            <div slot="header"><span>事件防抖动</span></div>
+                            <div class="lodash-content">
+                                this.$lodash.debounce(@click的事件,延迟的毫秒数)
+                                <br />
+                                // => 点击后多久不可以点击
+                            </div>
+                        </el-card>
+                    </el-col>
+                </el-row>
+            </section>
+
+
         </div>
     </article>
 </template>
@@ -55,24 +239,24 @@
         data() {
             return {
                 loading: false,
-                arr : [5,6,7,8,1,2,3,4,9,2,3,4,2,3,2],
-                arr1: [5,6,7,8,1,2,3,4,9,2,3,4,2,3,2],
-                arr2: [5,6,7,8,1,2,3,4,9,2,3,4,2,3,2],
-                arr3: [5,6,7,8,1,2,3,4,9,2,3,4,2,3,2],
-                string:'  1 2 3 4 5 6 7 8 9 0   ',
-                string1:'  1 2 3 4 5 6 7 8 9 0   ',
-                string2:'  1 2 3 4 5 6 7 8 9 0   ',
-                string3:'  1 2 3 4 5 6 7 8 9 0   ',
-                string4:'123456789012345678901234567890',
-                string5:'123456789012345678901234567890',
+                arr: [5, 6, 7, 8, 1, 2, 3, 4, 9, 2, 3, 4, 2, 3, 2],
+                arr1: [5, 6, 7, 8, 1, 2, 3, 4, 9, 2, 3, 4, 2, 3, 2],
+                arr2: [5, 6, 7, 8, 1, 2, 3, 4, 9, 2, 3, 4, 2, 3, 2],
+                arr3: [5, 6, 7, 8, 1, 2, 3, 4, 9, 2, 3, 4, 2, 3, 2],
+                string: '  1 2 3 4 5 6 7 8 9 0   ',
+                string1: '  1 2 3 4 5 6 7 8 9 0   ',
+                string2: '  1 2 3 4 5 6 7 8 9 0   ',
+                string3: '  1 2 3 4 5 6 7 8 9 0   ',
+                string4: '123456789012345678901234567890',
+                string5: '123456789012345678901234567890',
             };
         },
-        mounted(){
+        mounted() {
             /**
              * jQuery 相关的方法
              */
-            this.$nextTick( ()=>{
-                $("#jq").css('color','#f00');
+            this.$nextTick(() => {
+                $("#jq").css('color', '#f00');
             });
 
             /**
@@ -94,26 +278,48 @@
     };
 </script>
 <style scoped lang="scss">
-    #wrapper{
+    #wrapper {
         background-color: transparent;
     }
-    .section-breadcrumb{
+
+    .section-breadcrumb {
         margin-bottom: 20px;
     }
-    .section + .section{
+
+    .section+.section {
         margin-top: 20px;
     }
+
     .section-title {
         padding: 0 0 10px;
     }
-    .section-subhead{
+
+    .section-subhead {
         padding-bottom: 16px;
     }
-    .section-title + .section-subhead{
+
+    .section-title+.section-subhead {
         margin-top: -6px;
     }
 
-    .bgc{
+    .bgc {
         background-color: #f2f2f2;
+    }
+    .lodash-container {
+        text-align: left;
+        margin-top: 6px;
+        ::v-deep {
+            .lodash-content {
+                min-height: 150px;
+            }
+        }
+        .is-hover-shadow {
+            margin-bottom: 15px;
+        }
+    }
+</style>
+<style>
+    .body-bg-default{
+        background-color: #F0F0F0;
     }
 </style>
