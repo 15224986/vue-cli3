07dad60749b0e9f343ba117c927a97d4c0f4a93a
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-05-15 14:09:51 +0800

    Update jQuery.vue

--- src/views/index/jQuery.vue
+++ src/views/index/jQuery.vue
@@ -6,6 +6,49 @@
                 <moc-breadcrumb></moc-breadcrumb>
             </section>
 
+            <section class="section">
+                <h2>vue中style scope深度访问新方式(::v-deep)</h2>
+                <br />
+                <ul class="section-subhead">
+                    <li>1.参考网址： <el-link href="https://segmentfault.com/a/1190000021576348" type="primary" target="_blank">https://segmentfault.com/a/1190000021576348</el-link></li>
+                    <li>2.>>>   如果vue的style使用的是css，那么则使用 >>>, 但是像scss等预处理器却无法解析>>>，所以我们使用下面的方式.</li>
+                    <pre class="hljs xml">
+                        <code>
+                            <span class="hljs-tag">&lt;<span class="hljs-name">style</span> <span class="hljs-attr">lang</span>=<span class="hljs-string">"css"</span> <span class="hljs-attr">scoped</span>&gt;</span>
+                            .a &gt;&gt;&gt; .b {
+                               /* ... */
+                            }
+                            <span class="hljs-tag">&lt;/<span class="hljs-name">style</span>&gt;</span>
+                        </code>
+                    </pre>
+                    <li>3./deep/  但是有些开发者反应，在vue-cli3编译时，deep的方式会报错或者警告。此时我们可以使用第三种方式</li>
+                    <pre class="hljs xml">
+                        <code>
+                            <span class="hljs-tag">&lt;<span class="hljs-name">style</span> <span class="hljs-attr">lang</span>=<span class="hljs-string">"scss"</span> <span class="hljs-attr">scoped</span>&gt;</span>
+                                .a{
+                                   /deep/ .b {
+                                      /* ... */
+                                   }
+                                }
+                            <span class="hljs-tag">&lt;/<span class="hljs-name">style</span>&gt;</span>
+                        </code>
+                    </pre>
+                    <li>4.::v-deep  切记必须是双冒号</li>
+                    <pre class="hljs xml">
+                        <code>
+                            <span class="hljs-tag">&lt;<span class="hljs-name">style</span> <span class="hljs-attr">lang</span>=<span class="hljs-string">"scss"</span> <span class="hljs-attr">scoped</span>&gt;</span>
+                            .a{
+                               /deep/ .b {
+                                  /* ... */
+                               }
+                            }
+                            <span class="hljs-tag">&lt;/<span class="hljs-name">style</span>&gt;</span>
+                        </code>
+                    </pre>
+                </ul>
+
+            </section>
+
             <section class="section" v-mocTitle="$route.meta.title">
                 <h2 id="jq">测试是否引入了jq</h2>
                 <br />
@@ -322,4 +365,15 @@
     .body-bg-default{
         background-color: #F0F0F0;
     }
+    pre{
+        padding: 0;
+        line-height: 1.5;
+        background-color: #e9ecef;
+        overflow: auto
+    }
+    pre code{
+        overflow-wrap: normal;
+        white-space: inherit
+    }
+
 </style>
