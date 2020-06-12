5f88abcdde72f88b1dd2d86e692f9642f6dd462e
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-06-12 15:44:59 +0800

    Update common.js

--- src/mixins/common.js
+++ src/mixins/common.js
@@ -11,71 +11,6 @@ export default {
 			 */
             tableHeight: 100,           // 表格高度
 			tableIndexWidth: 40,        // 表格序号列的宽度
-			/**
-			 * 分页器相关设置
-			 */
-			paginationSet: {
-				sizes: [10, 20, 30, 40, 50],
-				layout: 'total, sizes, prev, pager, next, jumper'
-			},
-            /**
-             * 时间插件的右侧按钮
-             */
-            datePickerOptions: {
-                shortcuts: [{
-                    text: '最近一周',
-                    onClick (picker) {
-                        const end = new Date()
-                        const start = new Date()
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
-                        picker.$emit('pick', [start, end])
-                    }
-                }, {
-                    text: '最近一个月',
-                    onClick (picker) {
-                        const end = new Date()
-                        const start = new Date()
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
-                        picker.$emit('pick', [start, end])
-                    }
-                }, {
-                    text: '最近三个月',
-                    onClick (picker) {
-                        const end = new Date()
-                        const start = new Date()
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
-                        picker.$emit('pick', [start, end])
-                    }
-                }]
-            },
-            /**
-             * 选择月份范围 带快捷选项
-             *
-             * 做项目的时候，这个不需要放到公共文件里（保持公共的mixins要小）
-             */
-            datePickerMonth: {
-                shortcuts: [{
-                    text: '本月',
-                    onClick(picker) {
-                        picker.$emit('pick', [new Date(), new Date()]);
-                    }
-                }, {
-                    text: '今年至今',
-                    onClick(picker) {
-                        const end = new Date();
-                        const start = new Date(new Date().getFullYear(), 0);
-                        picker.$emit('pick', [start, end]);
-                    }
-                }, {
-                    text: '最近六个月',
-                    onClick(picker) {
-                        const end = new Date();
-                        const start = new Date();
-                        start.setMonth(start.getMonth() - 6);
-                        picker.$emit('pick', [start, end]);
-                    }
-                }]
-            },
 		}
 	},
 	watch: {
@@ -89,45 +24,6 @@ export default {
 		}
 	},
 	methods: {
-        /**
-         * 多层obj的值的获取和修改
-         * @param {Object} 最外层的obj
-         * @param {Object} 相对最外层的路径
-         * @param {Object} 是否开启严格模式
-         * @return {o} 最终字段所在的obj
-         * @return {k} 最终字段所在的obj的key
-         * @return {v} 获取值
-         *
-         *
-         *
-         * 做项目的时候，这个不需要放到公共文件里（保持公共的mixins要小）
-         */
-        getPropByPath(obj, path, strict) {
-            let tempObj = obj;
-            path = path.replace(/\[(\w+)\]/g, '.$1');
-            path = path.replace(/^\./, '');
-
-            let keyArr = path.split('.');
-            let i = 0;
-            for (let len = keyArr.length; i < len - 1; ++i) {
-                if (!tempObj && !strict) break;
-
-                let key = keyArr[i];
-                if (key in tempObj) {
-                    tempObj = tempObj[key];
-                } else {
-                    if (strict) {
-                        throw new Error('please transfer a valid prop path to form item!');
-                    }
-                    break;
-                }
-            }
-            return {
-                o: tempObj,
-                k: keyArr[i],
-                v: tempObj ? tempObj[keyArr[i]] : null
-            };
-        },
 		/**
 		 * 自定义索引
 		 */
