a97b4f399029f0af252537924892d6dedf06a4c8
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-06-17 19:32:13 +0800

    Update dialogDrag.js

--- src/directives/dialogDrag.js
+++ src/directives/dialogDrag.js
@@ -6,15 +6,15 @@ export default {
 		        const dialogHeaderEl = el.querySelector('.el-dialog__header');
 		        const dragDom = el.querySelector('.el-dialog');
 		        dialogHeaderEl.style.cursor = 'move';
-
+                dragDom.classList.add('moc-drag-dialog');
 		        // 获取原有属性 ie dom元素.currentStyle 火狐谷歌 window.getComputedStyle(dom元素, null);
 		        const sty = dragDom.currentStyle || window.getComputedStyle(dragDom, null);
-		        
+
 		        dialogHeaderEl.onmousedown = (e) => {
 		            // 鼠标按下，计算当前元素距离可视区的距离
 		            const disX = e.clientX - dialogHeaderEl.offsetLeft;
 		            const disY = e.clientY - dialogHeaderEl.offsetTop;
-		            
+
 		            // 获取到的值带px 正则匹配替换
 		            let styL, styT;
 
@@ -26,9 +26,9 @@ export default {
 		                styL = +sty.left.replace(/\px/g, '');
 		                styT = +sty.top.replace(/\px/g, '');
 		            };
-		            
+
 		            document.onmousemove = function (e) {
-		                // 通过事件委托，计算移动的距离 
+		                // 通过事件委托，计算移动的距离
 		                const l = e.clientX - disX;
 		                const t = e.clientY - disY;
 
@@ -49,7 +49,7 @@ export default {
 		                if(dragDomY<=0 && moveY<top){
 		                    moveY = top;
 		                }
-		                // 移动当前元素  
+		                // 移动当前元素
 		                dragDom.style.left = `${moveX}px`;
 		                dragDom.style.top = `${moveY}px`;
 		            };
@@ -58,16 +58,8 @@ export default {
 		                document.onmousemove = null;
 		                document.onmouseup = null;
 		            };
-		        }  
+		        }
 		    }
 		})
     }
 }
-
-
-
-
-
-
-
-
