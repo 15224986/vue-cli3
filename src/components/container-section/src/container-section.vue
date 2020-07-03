79ca0ed5d590974a47c8f83b621fa4855984ee58
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-07-03 14:00:35 +0800

    Add files via upload

---
+++ src/components/container-section/src/container-section.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="moc-container-section" :class="{ 'moc-container-bodier':bodier }">
+        <slot></slot>
+    </section>
+</template>
+
+<script>
+	export default{
+		name: 'mocContainerSection',
+		componentName: 'mocContainerSection',
+		data() {
+			return{
+			}
+		},
+	  	props: {
+            bodier: {
+                type: Boolean,
+                default: false
+            },
+		}
+	}
+</script>
