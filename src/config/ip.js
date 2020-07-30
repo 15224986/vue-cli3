2416be5ae437ef80fef194af0fa4b1413fb1d83f
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-07-30 13:48:18 +0800

    Update ip.js

--- src/config/ip.js
+++ src/config/ip.js
@@ -1,4 +1,31 @@
-export const baseURL = {
-    workOrderTenant: "http://10.4.66.83:80/cpap",   // 平台共享工单和租户入主工单
-    ip2: "http://10.4.66.45:8891/",             // 
+/**
+ * true 开发用， false 打包用
+ */
+const istest = true;
+
+
+
+/**
+ * 公共ip
+ */
+let axiosBaseURL = {
+    ip1: process.env.VUE_APP_URL,   // 平台共享工单和租户入主工单
+    ip2: "http://10.4.66.45:8891/",             //
+};
+if( istest ){
+    axiosBaseURL = {
+        ip1: '',   // 平台共享工单和租户入主工单
+        ip2: "http://10.4.66.45:8891/",             //
+    };
 }
+
+
+
+
+
+
+
+
+
+
+export { istest, axiosBaseURL };
