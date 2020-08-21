e34ea56745ce2e3a1604f47304a168546e6289f1
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-08-21 11:21:40 +0800

    Add files via upload

---
+++ src/views/container/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <el-container>
+    	<el-aside width="220px">
+            <el-menu
+                :default-active="$route.path"
+                class="el-menu-vertical-demo"
+                router
+            >
+                <el-menu-item index="/container/anextUntil/table">
+                    <span slot="title">表格</span>
+                </el-menu-item>
+                <el-menu-item index="/container/anextUntil/form">
+                    <span slot="title">表单</span>
+                </el-menu-item>
+
+    		</el-menu>
+    	</el-aside>
+        <el-main>
+    		<router-view></router-view>
+    	</el-main>
+    </el-container>
+</template>
+<script>
+
+    export default {
+        mixins:[],
+        components: {},
+        data () {
+			return {
+			}
+        },
+        created(){
+        },
+        mounted(){
+        },
+        methods:{
+        }
+    }
+</script>
+<style lang="scss">
+    .el-menu,
+    .el-aside {
+        color: #333;
+        border: 0 none;
+    }
+    .el-main{
+        padding: 0;
+    }
+</style>
