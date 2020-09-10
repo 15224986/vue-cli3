d6674f9b5c8ba95e1129403e98a376ebe1e435a5
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-09-10 09:12:28 +0800

    Add files via upload

---
+++ src/store/getters.js
@@ -0,0 +1,18 @@
+const getters = {
+    toFullPath: state => state.toFullPath,
+    renderSrc: state => state.toFullPath.src,
+
+    userInfo: state => state.userInfo,
+    user: state => state.userInfo.user,
+    token: state => state.userInfo.token,
+    refreshToken: state => state.userInfo.refreshToken,
+    tokenExpire: state => state.userInfo.tokenExpire,
+    menus: state => state.userInfo.menus,
+    elements: state => state.userInfo.elements,
+    maxNumber: state => {
+        return state.userInfo.number.filter(function(item, index) {
+            return item >= 5;
+        });
+    }
+}
+export default getters
