c6eeb78bdda884b276de9ad5a09ab71cd8fd629b
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-09-10 09:05:40 +0800

    Add files via upload

---
+++ src/utils/auth.js
@@ -0,0 +1,36 @@
+const TokenKey = 'Admin-Token'
+const RefreshTokenKey = 'Admin-Token-Refresh'
+const TokenExpire = 'Admin-Token-expire'
+
+
+export function setToken(token) {
+    sessionStorage.setItem(TokenKey, token);
+}
+export function getToken() {
+    return sessionStorage.getItem(TokenKey)
+}
+export function removeToken() {
+    sessionStorage.removeItem(TokenKey)
+}
+
+
+export function setTokenExpire(tokenExpire) {
+    sessionStorage.setItem(TokenExpire, tokenExpire);
+}
+export function getTokenExpire() {
+    return sessionStorage.getItem(TokenExpire)
+}
+export function removeTokenExpire() {
+    sessionStorage.removeItem(TokenExpire)
+}
+
+
+export function setRefreshToken(refreshToken) {
+    sessionStorage.setItem(RefreshTokenKey, refreshToken);
+}
+export function getRefreshToken() {
+    return sessionStorage.getItem(RefreshTokenKey)
+}
+export function removeRefreshToken() {
+    sessionStorage.removeItem(RefreshTokenKey)
+}
