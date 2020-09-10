49b258ade4a878e80fd718f96740f97998d348a6
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-09-10 08:47:12 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,50 +4,9 @@
     </div>
 </template>
 <script>
-
-    import { mapState, mapGetters, mapActions } from 'vuex';
-
     // 加载资源
     export default {
-        name: 'App',
-        components: {
-        },
-        created () {
-            // 在页面加载时读取sessionStorage里的状态信息,如果存在证明是刷新后进入的
-            if (sessionStorage.getItem("store")) {
-                this.$store.replaceState( Object.assign( {}, this.$store.state, JSON.parse(sessionStorage.getItem("store")) ) );
-                sessionStorage.removeItem("store");
-                if( this.userInfo ){
-                    this.$router.push({ path: this.renderSrc || '/' });
-                    this.changeSrc(false);
-                }
-            }
-            // 在页面刷新时将vuex里的信息保存到sessionStorage里
-            window.addEventListener("beforeunload", ()=>{
-                // 下一次将要跳转的页面
-                let fullPath = this.$route.fullPath;
-                if( fullPath === '/login' ){ // 当前刷新页面为登录页时
-                    if( this.renderSrc ){
-                        fullPath = this.renderSrc;
-                    }else{
-                        fullPath = '/';
-                    }
-                }
-                this.changeSrc( fullPath );
-                sessionStorage.setItem("store", JSON.stringify(this.$store.state) );
-            });
-        },
-        computed:{
-            ...mapGetters({
-                renderSrc: 'toFullPath/renderSrc',    // toFullPath 模块下，getters里面的renderSrc方法
-                userInfo: 'userInfo/renderUser'
-            })
-        },
-        methods:{
-            ...mapActions({
-                changeSrc: 'toFullPath/invokeChangeSrc'
-            })
-        }
+        name: 'App'
     }
 </script>
 <style lang="scss">
