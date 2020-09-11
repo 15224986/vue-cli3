40a0beacb31a49406a356cf6a1975fc0ac86cc2b
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-09-11 09:51:04 +0800

    Add files via upload

---
+++ src/store/modules/app.js
@@ -0,0 +1,35 @@
+import {
+    setSidebarOpened,
+    getSidebarOpened
+} from '@/utils/auth'
+import toBoolean from '@/utils/toBoolean'
+
+const state = {
+    sidebar: {
+        opened: toBoolean(getSidebarOpened()) ? toBoolean(getSidebarOpened()) : false
+    }
+}
+
+const mutations = {
+    TOGGLE_SIDEBAR: state => {
+        state.sidebar.opened = !state.sidebar.opened
+        if (state.sidebar.opened) {
+           setSidebarOpened(true)
+        } else {
+           setSidebarOpened(false)
+        }
+    }
+}
+
+const actions = {
+    toggleSideBar({commit}) {
+        commit('TOGGLE_SIDEBAR')
+    }
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions
+}
