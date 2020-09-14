230c20e3e64ec52626457a8a7c37d2850945ce15
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-09-14 19:40:16 +0800

    Update app.js

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -2,22 +2,19 @@ import {
     setSidebarOpened,
     getSidebarOpened
 } from '@/utils/auth'
-import toBoolean from '@/utils/toBoolean'
+
+
 
 const state = {
     sidebar: {
-        opened: toBoolean(getSidebarOpened()) ? toBoolean(getSidebarOpened()) : false
+        opened: getSidebarOpened()
     }
 }
 
 const mutations = {
     TOGGLE_SIDEBAR: state => {
         state.sidebar.opened = !state.sidebar.opened
-        if (state.sidebar.opened) {
-           setSidebarOpened(true)
-        } else {
-           setSidebarOpened(false)
-        }
+        setSidebarOpened(state.sidebar.opened)
     }
 }
 
