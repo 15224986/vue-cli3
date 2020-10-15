d2a97b7c5b3ed90c7e437cbea1d77736018bbce9
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-10-15 17:06:52 +0800

    Add files via upload

---
+++ src/views/500.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>500</h1>
+</template>
+<script>
+    export default {
+        data () {
+			return {
+			}
+        },
+        created(){
+        },
+        mounted(){
+        },
+        methods:{
+        }
+    }
+</script>
