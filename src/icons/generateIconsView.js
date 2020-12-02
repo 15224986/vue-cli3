326759530ba022fd77250d4a5d08ccc4b9958b2c
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-12-02 16:23:23 +0800

    Add files via upload

---
+++ src/icons/generateIconsView.js
@@ -0,0 +1,10 @@
+const generateIcons = {
+    data: [],
+    generate(iconsMap) {
+        this.data = iconsMap.map((i) => {
+            return i.default.id.substring(5)
+        })
+    }
+}
+
+export default generateIcons
---
+++ src/icons/index.js
@@ -0,0 +1,13 @@
+// 默认导入所有src文件夹下的icons文件(自动导入，不需手动一个个导入)
+import Vue from "vue";
+import SvgIcon from "./SvgIcon";    //写了组件在打开 用于注册组件
+
+
+Vue.component("svg-icon", SvgIcon);//写了组件在打开 用于注册组件
+const requireAll = requireContext => requireContext.keys().map(requireContext);
+const req = require.context('./svg', false, /\.svg$/);
+const iconMap = requireAll(req);
+
+
+import generateIconsView from './generateIconsView.js'
+generateIconsView.generate(iconMap)
