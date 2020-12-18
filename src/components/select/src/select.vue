9c0d8ff3ad563844aaf456138d1ee70544d9fe2e
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2020-12-18 14:53:13 +0800

    Update select.vue

--- src/components/select/src/select.vue
+++ src/components/select/src/select.vue
@@ -1,114 +1,191 @@
 <template>
-	<div class="moc-select-box">
-		<div class="moc-select-content" :class="{ 'open': optionsShow, 'disabled': disabled, 'has-valua': clearableType }">
-			<input class="input" :disabled="disabled" @click="optionsToggle" v-model="activeText" readonly :placeholder="placeholder" />
-			<span class="arrow"></span>
-			<span class="clear-btn" v-if="clearable" @click="clearablefn">X</span>
-		</div>
-		<transition name="slide-down">
-			<ul class="moc-select-options" v-show="optionsShow">
-				<li class="moc-select-option"
-					v-for="(item, index) in options"
-					:key="index"
-					@click="sleectItem(item,index)"
-					:class="{'active':selectedVal===item.value, 'disabled':item.disabled}"
-				>{{ item.label }}</li>
-			</ul>
-		</transition>
-	</div>
+    <div class="moc-cascader" v-clickoutside="handleClose">
+        <el-popover trigger="manual" v-model="visible" :width="width" placement="bottom-start" popper-class="moc-cascader-popover">
+            <div class="moc-cascader-menu" :style="{ height: height+'px' }">
+                <div class="moc-cascader-list">
+                    <el-input v-model="firstSearch" suffix-icon="el-icon-search" :placeholder="title[0]" class="moc-cascader-search"></el-input>
+                    <ul class="moc-cascader-group">
+                        <li
+                            v-for="(item, index) in firstOptions" :key="index"
+                            :class="{'is-active': firstValue == item.value }"
+                            @click.stop="onClickFirstList( item )"
+                        >
+                            <i :class="item.children && item.children.length>0 ? 'el-icon-folder': 'el-icon-document'"></i>{{item.label}}
+                        </li>
+                    </ul>
+                </div>
+                <div class="moc-cascader-list">
+                    <el-input v-model="secondSearch" suffix-icon="el-icon-search" :placeholder="title[1]" class="moc-cascader-search"></el-input>
+                    <ul class="moc-cascader-group">
+                        <li
+                            v-for="(item, index) in secondOptions" :key="index"
+                            :class="{'is-active': secondValue == item.value }"
+                            @click.stop="onClickSecondList( item )"
+                        >
+                            <i :class="item.children && item.children.length>0 ? 'el-icon-folder': 'el-icon-document'"></i>{{item.label}}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <el-input
+                v-model="cascaderLabel"
+                @focus="handleOpen"
+                @keydown.native.esc.stop.prevent="visible = false"
+                @keydown.native.tab="visible = false"
+                slot="reference"
+                :suffix-icon=" visible ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"
+            ></el-input>
+        </el-popover>
+    </div>
 </template>
 
 <script>
-	export default{
-		name: 'mocSelect',
+    import Clickoutside from '../../utils/clickoutside';
+
+    export default {
+        name: 'mocSelect',
         componentName: 'mocSelect',
-		data() {
-			return{
-				optionsShow: false
-			}
-		},
-		// 获取父组件通过v-model传过来的值
-		model: {
-	    	prop: 'selectedVal', 	// 绑定到的位置
-	    	event: 'input'			// 通过什么事件改变
-	  	},
-	  	props: {
-	    	selectedVal: {
-		    	type: [String,Array],
-		     	default: ''
-		    },
-		    options:{
-		    	type: Array,
-			    // 对象或数组默认值必须从一个工厂函数获取
-			    default: function () {
-			    	return []
-			    }
-		    },
-		    // 带有默认值的数字
-		    disabled: {
-		    	type: Boolean,
-		     	default: false
-		    },
-		    clearable: Boolean,
-		    placeholder: {
-		    	type: String,
-		     	default: '请选择'
-		    }
-		},
-		computed: {
-			activeText: function () {
-				var activeTxt = '';
-                for (var i = 0; i < this.options.length; i++) {
-                	if(this.selectedVal == this.options[i].value){
-                		return this.options[i].label;
-                	}
+        props: {
+            value: {
+                type: [String, Number],
+                default: ""
+            },
+            options: {
+                type: Array,
+                default: () => []
+            },
+            width: {
+                type: [String, Number],
+                default: "420"
+            },
+            height: {
+                type: [String, Number],
+                default: "280"
+            },
+            title: {
+                type: Array,
+                default: () => ['分组', '标准']
+            },
+            separator:{
+                type: String,
+                default: "-"
+            },
+
+            selectOptions: {
+                type: Array,
+                // default: function () { return [] }
+                default: () => []
+            },
+        },
+        data() {
+            return {
+                visible: false,
+                /**
+                 * 值
+                 */
+                cascaderLabel:"",
+                /**
+                 * 第一
+                 */
+                firstSearch: "",
+                firstValue: "",
+                firstLabel: "",
+                /**
+                 * 第二
+                 */
+                secondSearch: "",
+                secondValue: "",
+                secondLabel: "",
+                secondData:[]
+            }
+        },
+        computed: {
+            /**
+             * 输出字段搜索的方法
+             */
+            firstOptions: function(){
+                return this.options.filter( (item)=> {
+                    if( item.label == null ){
+                        item.label = '';
+                    }
+                    if( item.label.search(this.firstSearch) !== -1 ){
+                        return item
+                    }
+                })
+            },
+            secondOptions: function(){
+                return this.secondData && this.secondData.filter( (item)=> {
+                    if( item.label == null ){
+                        item.label = '';
+                    }
+                    if( item.label.search(this.secondSearch) !== -1 ){
+                        return item
+                    }
+                })
+            }
+        },
+        watch: {
+            value:{
+                immediate: true,
+                handler(val, oldName) {
+                    this.initData(val)
                 }
             },
-            clearableType: function () {
-				if( typeof this.selectedVal === 'string' ){
-					if( this.selectedVal.replace(/^\s+|\s+$/g,"") ){
-						return true;
-					}else{
-						return false;
-					}
-				}else{
-					if( this.selectedVal.length>0 ){
-						return true;
-					}else{
-						return false;
-					}
-				}
+        },
+        directives: { Clickoutside },
+        methods: {
+            // 展开
+            handleOpen(){
+                if( this.visible === false ){
+                    this.visible = true;
+                    this.initData( this.value );
+                }
+            },
+            // 关闭
+            handleClose(){
+                this.visible = false;
+            },
+            // 一级点击
+            onClickFirstList(item){
+                if( item.children && item.children.length>0 ){
+                    this.firstValue = item.value
+                    this.secondData = item.children
+                }else{
+                    this.$emit('input', item.value);
+                    this.handleClose();
+                }
+            },
+            // 二级点击
+            onClickSecondList(item){
+                this.secondValue = item.value
+                let Value = this.firstValue + this.separator + item.value
+                this.$emit('input', Value);
+                this.handleClose();
+            },
+            /**
+             * 初始化数据
+             */
+            initData(val){
+                let valueArray = val.split( this.separator );
+                this.options.forEach(item => {
+                    if( valueArray[0] == item.value ){
+                        this.firstValue = item.value
+                        this.firstLabel = item.label
+                        this.secondData = item.children
+                    }
+                });
+                if( valueArray[1] ){
+                    this.secondData.forEach(item => {
+                        if( valueArray[1] == item.value ){
+                            this.secondValue = item.value
+                            this.secondLabel = item.label
+                            this.cascaderLabel = this.firstLabel + this.separator + item.label
+                        }
+                    });
+                }else{
+                    this.cascaderLabel = this.firstLabel
+                }
             }
-		},
-		methods:{
-			sleectItem(item,index){
-				if( item.disabled ){
-					return false;
-				}
-				this.activeIndex = index;
-				this.optionsShow = !this.optionsShow;
-
-				var val = item.value;
-				/**
-				 * 传给父组件绑定的v-model值的变化
-				 */
-				this.$emit('input', val);
-				/**
-				 * 给组件定义“change”事件，传递两个参数，分别是index和val
-				 */
-				this.$emit('change', item, index);
-			},
-			optionsToggle(){
-				if( this.disabled ){
-					return false;
-				}
-				this.optionsShow = !this.optionsShow;
-			},
-			clearablefn(){
-				var val = '';
-				this.$emit('input', val);
-				this.optionsShow = false;
-			}
-		}
-	}
+        },
+    }
 </script>
