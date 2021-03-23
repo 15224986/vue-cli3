80508e846592f7e8bdd2073f550cb73ec4615d89
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-23 11:10:35 +0800

    Add files via upload

---
+++ src/directives/index.js
@@ -0,0 +1,14 @@
+import mocStealth from './modules/stealth'
+import mocClickOutside from './modules/clickoutside'
+import mocTitle from './modules/title'
+import mocDialogDrag from './modules/dialogDrag'
+
+
+
+
+export {
+    mocStealth,
+    mocClickOutside,
+    mocTitle,
+    mocDialogDrag
+}
---
+++ src/directives/modules/title.js
@@ -0,0 +1,6 @@
+// 元素隐藏显示指令
+export default {
+    bind(el, binding, vnode) {
+        el.title = binding.value
+    }
+}
