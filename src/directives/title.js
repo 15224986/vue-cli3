8ff1cb2c6f1f495539c1a88d12f02e3b2a26a32d
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-03-23 03:11:00 +0000

    Delete title.js

--- src/directives/title.js
+++
@@ -1,9 +0,0 @@
-export default {
-    install: function (Vue, options) {
-        Vue.directive("mocTitle", {
-            bind(el, binding, vnode) {
-                el.title = binding.value
-            }
-        })
-    }
-}
