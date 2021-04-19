c253bef16d1b6164bf532bb1d4ec6083e4b2a8e1
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-04-19 17:05:06 +0800

    Update index.vue

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,231 +1,288 @@
-<template>
-    <article id="page-bodier" :class="{'neu-sidebar-opened': sidebar.opened}">
-        <aside id="sidebar">
-            <div class="neu-menu-tools">
-                <svg @click="toggleSideBar()" class="hamburger" :class="{'neu-active': !sidebar.opened}" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="64" height="64">
-                    <path d="M408 442h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm-8 204c0 4.4 3.6 8 8 8h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56zm504-486H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 632H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM142.4 642.1L298.7 519a8.84 8.84 0 0 0 0-13.9L142.4 381.9c-5.8-4.6-14.4-.5-14.4 6.9v246.3a8.9 8.9 0 0 0 14.4 7z"></path>
-                </svg>
-            </div>
-
-            <el-menu
-                :default-active="$route.path"
-                :collapse="sidebar.opened"
-                class="el-menu-vertical-demo"
-                @open="handleOpen"
-                @close="handleClose"
-                router
-            >
-
-
-                <el-menu-item index="/home/jQuery">
-                    <i class="el-icon-menu"></i>
-                    <el-tooltip slot="title" content="jQuery 全局引用，Array、String 原型链添加方法" placement="right">
-                        <span>jQuery、Array、String</span>
-                    </el-tooltip>
-                </el-menu-item>
-
-
-
-                <el-menu-item index="/home/verify">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">验证码</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/directiveFilters">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">全局指令、过滤器、工具</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/orgchart">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">orgchart组织架构图</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/tinymce" :route="{'path':'/home/tinymce', query:{'title':'富文本编辑器', id:'151'}}">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">tinymce5富文本编辑器</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/sector">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">css3 效果</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/syncModel">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">数据的双向传递</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/multilayerNesting">
-                    <i class="el-icon-menu"></i>
-                    <el-tooltip slot="title" content="Vue多层嵌套、utils公共方法" placement="right">
-                        <span>Vue多层嵌套、utils公共方法</span>
-                    </el-tooltip>
-                </el-menu-item>
-
-                <el-menu-item index="/home/complexData">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">js复杂数据类型的深层复制</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/fuzzySearch">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">模糊搜索、文字滚动</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/tableRelevant">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">table相关</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/exportExcel">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">导出excel</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/sortablejs">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">拖拽排序</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/draggableResizable">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">可拖拽可缩放</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/mockjsData">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">mock.js测试</span>
-                </el-menu-item>
-
-                <el-menu-item index="/home/lotteryDraw">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">抽奖功能</span>
-                </el-menu-item>
-
-
-                <el-submenu index="1">
-                    <template slot="title">
-                        <i class="el-icon-menu"></i>
-                        <span>element-ui 用法</span>
-                    </template>
-
-                    <el-menu-item index="/home/elementCustomParams">
-                        <span slot="title">Element 自定义参数</span>
-                    </el-menu-item>
-
-                    <el-menu-item index="/home/selectRelevant">
-                        <span slot="title">select全选、联动</span>
-                    </el-menu-item>
-
-                    <el-menu-item index="/home/requiredType">
-                        <span slot="title">切换必填状态、tooltip使用</span>
-                    </el-menu-item>
-
-                    <el-menu-item index="/home/transferSearch">
-                        <el-tooltip slot="title" content="穿梭框自定义搜索、图片放大" placement="right">
-                            <span>穿梭框自定义搜索、图片放大</span>
-                        </el-tooltip>
-                    </el-menu-item>
-                </el-submenu>
-
-
-
-                <el-submenu index="2">
-                    <template slot="title">
-                        <i class="el-icon-menu"></i>
-                        <span>通过View UI补充的组件</span>
-                    </template>
-
-                    <el-menu-item index="/home/anchor">
-                        <span slot="title">Anchor锚点、Affix图钉</span>
-                    </el-menu-item>
-
-                    <el-menu-item index="/home/sliderScale">
-                        <span slot="title">滑块缩放、面板分割</span>
-                    </el-menu-item>
-                </el-submenu>
-
-            </el-menu>
-        </aside>
-        <main id="content">
-            <moc-tags-view></moc-tags-view>
-            <transition  name="fade-transform" mode="out-in">
-                <keep-alive>
-                    <router-view :key="key"></router-view>
-                </keep-alive>
-            </transition>
-        </main>
-    </article>
-</template>
-<script>
-
-    import { mapState, mapGetters, mapActions } from 'vuex';
-    export default {
-        name: 'widget',
-        data() {
-            return {};
+export default {
+    path: 'home',
+	component: () => import(/* webpackChunkName: "home" */ '@/views/index/index'),
+    name: 'case',
+    redirect: '/home/jQuery',
+    meta: {
+		title: "案例页面",
+		path: '/home/jQuery'
+	},
+    children: [
+        {
+        	path: 'shortcut',
+        	component: () => import(/* webpackChunkName: "home" */ '@/views/index/shortcut'),
+        	name: 'shortcut',
+        	meta: {
+        		loginRrquired: false,
+        		keepAlive: true,
+        		title: "监听键盘操作（组合键）",
+        		path: '/home/shortcut',
+        		affix: false
+        	}
         },
-        computed: {
-            key() {
-                console.log(this.$route)
-                if(this.$route.meta.keepAlive){
-                    return this.$route.fullPath
-                }else{
-                    return this.$route.fullPath + new Date().getTime()
-                }
-                // return this.$route.fullPath.replace(/[/?=&]/g, '')
-            },
-            ...mapGetters([
-                'sidebar'
-            ])
+        {
+        	path: 'elementCustomParams',
+        	component: () => import(/* webpackChunkName: "home" */ '@/views/index/elementCustomParams'),
+        	name: 'elementCustomParams',
+        	meta: {
+        		loginRrquired: false,
+        		keepAlive: true,	// 是否使用keep-alive缓存页面
+        		title: "Element 自定义参数",
+        		path: '/home/elementCustomParams',
+        		affix: false //
+        	}
         },
-        mounted() {
-           console.log(this.sidebar.opened, this.$store.getters.sidebar.opened)
+        {
+        	path: 'jQuery',
+        	component: () => import(/* webpackChunkName: "home" */ '@/views/index/jQuery'),
+        	name: 'jQuery',
+        	meta: {
+        		loginRrquired: false,
+        		keepAlive: true,
+        		title: "jQuery 引用",
+        		path: '/home/jQuery',
+        		affix: true
+        	}
         },
-        methods: {
-            toggleSideBar() {
-                this.$store.dispatch('app/toggleSideBar').then(() => {
-                    console.log( this.sidebar.opened )
-                })
-            },
-            handleOpen(key, keyPath) {
-                console.log(key, keyPath);
-            },
-            handleClose(key, keyPath) {
-                console.log(key, keyPath);
-            },
-        }
-    };
-</script>
-<style lang="scss">
-    #content{
-        overflow: hidden
-    }
-    /* 可以设置不同的进入和离开动画 */
-    /* 设置持续时间和动画函数 */
-    .fade-transform-enter-active,.fade-transform-leave-active {
-        -webkit-transition: all .5s;
-        transition: all .5s
-    }
-    .fade-transform-enter {
-        opacity: 0;
-        -webkit-transform: translateX(-30px);
-        transform: translateX(-30px)
-    }
-    .fade-transform-leave-to {
-        opacity: 0;
-        -webkit-transform: translateX(30px);
-        transform: translateX(30px)
-    }
-
-    .el-menu-item{
-        overflow: hidden;
-        white-space:nowrap;
-        .el-tooltip{
-            display: inline-block;
-            width: calc(100% - 30px);
-            overflow: hidden;
-        }
-    }
-</style>
+    	{
+			path: 'anchor',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/anchor'),
+			name: 'anchor',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,	// 是否使用keep-alive缓存页面
+				title: "Anchor锚点、Affix图钉",
+				path: '/home/anchor',
+				affix: false //
+			}
+		},
+        {
+        	path: 'verify',
+        	component: () => import(/* webpackChunkName: "home" */ '@/views/index/verify'),
+        	name: 'verify',
+        	meta: {
+        		loginRrquired: false,
+        		keepAlive: true,	// 是否使用keep-alive缓存页面
+        		title: "验证码",
+        		path: '/home/verify',
+        		affix: false //
+        	}
+        },
+    	{
+			path: 'directiveFilters',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/directive-filters'),
+			name: 'directive-filters',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "指令、过滤器、工具的全局使用",
+				path: '/home/directiveFilters',
+				affix: false //
+			}
+		},
+    	{
+			path: 'orgchart',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/orgchart'),
+			name: 'orgchart',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "orgchart组织架构图",
+				path: '/home/orgchart',
+				affix: false
+			}
+		},
+    	{
+			path: 'tinymce',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/tinymce'),
+			name: 'tinymce',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "tinymce5富文本编辑器",
+				path: '/home/tinymce',
+				affix: false
+			}
+		},
+		{
+			path: 'sector',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/sector'),
+			name: 'sector',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "css3 效果",
+				path: '/home/sector',
+				affix: false
+			}
+		},
+		{
+			path: 'syncModel',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/syncModel'),
+			name: 'syncModel',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "数据的双向传递",
+				path: '/home/syncModel',
+				affix: false
+			}
+		},
+		{
+			path: 'multilayerNesting',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/multilayerNesting'),
+			name: 'multilayerNesting',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "Vue多层嵌套",
+				path: '/home/multilayerNesting',
+				affix: false
+			}
+		},
+		{
+			path: 'selectRelevant',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/selectRelevant'),
+			name: 'selectRelevant',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "select全选、联动",
+				path: '/home/selectRelevant',
+				affix: false
+			}
+		},
+		{
+			path: 'complexData',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/complexData'),
+			name: 'complexData',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "js复杂数据类型的深层复制",
+				path: '/home/complexData',
+				affix: false
+			}
+		},
+		{
+			path: 'transferSearch',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/transferSearch'),
+			name: 'transferSearch',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "穿梭框自定义搜索",
+				path: '/home/transferSearch',
+				affix: false
+			}
+		},
+		{
+			path: 'requiredType',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/requiredType'),
+			name: 'requiredType',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "切换必填状态",
+				path: '/home/requiredType',
+				affix: false
+			}
+		},
+		{
+			path: 'fuzzySearch',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/fuzzySearch'),
+			name: 'fuzzySearch',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "模糊搜索",
+				path: '/home/fuzzySearch',
+				affix: false
+			}
+		},
+		{
+			path: 'tableRelevant',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/tableRelevant'),
+			name: 'tableRelevant',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "table相关",
+				path: '/home/tableRelevant',
+				affix: false
+			}
+		},
+        {
+        	path: 'exportExcel',
+        	component: () => import(/* webpackChunkName: "home" */ '@/views/index/exportExcel'),
+        	name: 'exportExcel',
+        	meta: {
+        		loginRrquired: false,
+        		keepAlive: true,
+        		title: "导出excel",
+        		path: '/home/exportExcel',
+        		affix: false
+        	}
+        },
+		{
+			path: 'sortablejs',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/sortablejs'),
+			name: 'sortablejs',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "拖拽排序",
+				path: '/home/sortablejs',
+				affix: false
+			}
+		},
+        {
+        	path: 'draggableResizable',
+        	component: () => import(/* webpackChunkName: "home" */ '@/views/index/draggableResizable'),
+        	name: 'draggableResizable',
+        	meta: {
+        		loginRrquired: false,
+        		keepAlive: true,
+        		title: "可拖拽可缩放",
+        		path: '/home/draggableResizable',
+        		affix: false
+        	}
+        },
+		{
+			path: 'mockjsData',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/mockjsData'),
+			name: 'mockjsData',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "mock.js测试",
+				path: '/home/mockjsData',
+				affix: false
+			}
+		},
+		{
+			path: 'lotteryDraw',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/lotteryDraw'),
+			name: 'lotteryDraw',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "抽奖功能",
+				path: '/home/lotteryDraw',
+				affix: false
+			}
+		},
+		{
+			path: 'sliderScale',
+			component: () => import(/* webpackChunkName: "home" */ '@/views/index/sliderScale'),
+			name: 'sliderScale',
+			meta: {
+				loginRrquired: false,
+				keepAlive: true,
+				title: "滑块控制缩放",
+				path: '/home/sliderScale',
+				affix: false
+			}
+		}
+	]
+}
