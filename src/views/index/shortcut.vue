558e479591ff9cb4abccdd46fa8bfeb2b6eac548
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-04-19 17:06:41 +0800

    Add files via upload

---
+++ src/views/index/shortcut.vue
@@ -0,0 +1,65 @@
+<template>
+    <article id="page-bodier">
+        <div id="wrapper" v-loading="loading">
+
+            <section class="section m-t-20">
+                <h2>同时按下了 Ctrl+Alt+1</h2>
+                <p>执行次数：{{ctrlAlt1Num}}</p>
+            </section>
+
+            <section class="section">
+                <h2>同时按下了 Ctrl+Alt+f1</h2>
+                <p>执行次数：{{ctrlAltf1Num}}</p>
+            </section>
+
+
+        </div>
+    </article>
+</template>
+<script>
+
+    import { shortcut, keyCodeForEvent } from '@/utils/shortcut.js'
+
+    export default {
+
+        data() {
+            return {
+                loading: false,
+                ctrlAlt1Num:0,
+                ctrlAltf1Num:0
+            };
+        },
+        mounted() {
+            keyCodeForEvent( 49, ()=>{
+                this.$message('同时按下了 Ctrl+Alt+1')
+                 this.ctrlAlt1Num++
+            });
+            shortcut.add("Ctrl+Alt+f1", ()=>{
+                this.$message('同时按下了 Ctrl+Alt+f1');
+                 this.ctrlAltf1Num++
+            });
+        },
+        methods:{
+        }
+    };
+</script>
+<style scoped lang="scss">
+    #wrapper {
+        background-color: transparent;
+    }
+    .section-breadcrumb {
+        margin-bottom: 20px;
+    }
+    .section+.section {
+        margin-top: 20px;
+    }
+    .section-title {
+        padding: 0 0 10px;
+    }
+    .section-subhead {
+        padding-bottom: 16px;
+    }
+    .section-title+.section-subhead {
+        margin-top: -6px;
+    }
+</style>
