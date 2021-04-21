f198ef00efecb23491615b8d40bea5589ddfc3e8
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-04-21 16:26:25 +0800

    Update shortcut.vue

--- src/views/index/shortcut.vue
+++ src/views/index/shortcut.vue
@@ -1,6 +1,45 @@
 <template>
     <article id="page-bodier">
         <div id="wrapper" v-loading="loading">
+            <section class="section m-t-20">
+                <h2>多选</h2>
+                <div class="neu-select-group">
+                    <checkbox-radio
+                        v-for="item in options" :key="item.value"
+                        type='checkbox'
+                        v-model="checkbox"
+                        :label='item.value'
+                        :disabled=" item.disabled"
+                    >
+                        {{item.label}}
+                    </checkbox-radio>
+                </div>
+                <p>值：{{checkbox.join(',')}}</p>
+                <div class="neu-select-group">
+                    <checkbox-radio
+                        v-for="item in options" :key="item.value"
+                        type='checkbox'
+                        v-model="item.checked"
+                        :label='item.value'
+                        :disabled="item.disabled"
+                    >
+                        {{item.label}}:{{item.checked}}
+                    </checkbox-radio>
+                </div>
+                <h2>单选</h2>
+                <div class="neu-select-group">
+                    <checkbox-radio
+                        v-for="item in options" :key="item.value"
+                        type='radio'
+                        v-model="radio"
+                        :label='item.value'
+                        :disabled=" item.disabled"
+                    >
+                        {{item.label}}
+                    </checkbox-radio>
+                </div>
+                <p>{{radio}}</p>
+            </section>
 
             <section class="section m-t-20">
                 <h2>同时按下了 Ctrl+Alt+1</h2>
@@ -21,13 +60,60 @@
     import { shortcut, keyCodeForEvent } from '@/utils/shortcut.js'
 
     export default {
-
+        components: {
+            checkboxRadio:()=>import('./checkbox-radio.vue')
+        },
         data() {
             return {
                 loading: false,
                 ctrlAlt1Num:0,
-                ctrlAltf1Num:0
+                ctrlAltf1Num:0,
+
+                checkbox:[1, 3],
+                radio: '2',
+                options:[
+                    {
+                        label: '北京',
+                        value: 1,
+                        checked: false
+                    },
+                    {
+                        label: '上海',
+                        value: 2,
+                        checked: true
+                    },
+                    {
+                        label: '广州',
+                        value: 3,
+                        checked: false
+                    },
+                    {
+                        label: '深圳',
+                        value: 4,
+                        checked: true
+                    },
+                    {
+                        label: '天津',
+                        value: 5,
+                        disabled: true,
+                        checked: false
+                    },
+                    {
+                        label: '重庆',
+                        value: 6,
+                        checked: false
+                    }
+                ]
             };
+        },
+        computed: {
+            isChecked() {
+                // if(Array.isArray(this.model)) {
+                //     return this.checkbox.includes(item.value);
+                // }else{
+                //     return item.value;
+                // }
+            }
         },
         mounted() {
             keyCodeForEvent( 49, ()=>{
@@ -40,6 +126,7 @@
             });
         },
         methods:{
+
         }
     };
 </script>
@@ -61,5 +148,11 @@
     }
     .section-title+.section-subhead {
         margin-top: -6px;
+    }
+
+
+    .neu-select-group{
+        @include clearfix;
+        padding: 20px 0;
     }
 </style>
