fccc9fe0509047473328a67fda9f89357b9f7598
vue-cli3-blank <35017110+15224986@users.noreply.github.com>
2021-05-10 09:59:35 +0800

    Add files via upload

---
+++ src/components/more-panel/src/main.vue
@@ -0,0 +1,53 @@
+<template>
+	<section class="moc-more-panel">
+        <div class="moc-more-panel-tools" v-if="$scopedSlots.tools">
+            <slot name="tools"></slot>
+        </div>
+        <div class="more-panel-body" :style="{'height': height }">
+            <slot></slot>
+        </div>
+        <div class="more-panel-toggle" @click="handleToggle()">
+            <i :class="isOpen ? 'el-icon-arrow-down':'el-icon-arrow-up'"></i>
+        </div>
+    </section>
+</template>
+
+<script>
+	export default{
+		name: 'mocMorePanel',
+		componentName: 'mocMorePanel',
+        props: {
+            originalHeight: {
+                type: [Number,String],
+                default: 34
+            },
+            isOpen: {
+                type: Boolean,
+                default: false
+            }
+        },
+		data() {
+			return{
+			}
+		},
+        computed: {
+            height() {
+                if( this.isOpen ){
+                    return 'auto'
+                }else{
+                    if( isNaN(this.originalHeight) ){
+                        return this.originalHeight
+                    }else{
+                        return this.originalHeight + 'px'
+                    }
+                }
+
+            }
+        },
+        methods:{
+            handleToggle(){
+                this.$emit('update:isOpen', !this.isOpen );
+            }
+        }
+	}
+</script>
